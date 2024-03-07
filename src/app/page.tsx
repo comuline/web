37e2a3630d0913f5b6f622ed586336975089b48d
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-03-07 15:53:56 +0700

    metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
+import MainPage from "@/ui/pages/home";
+
 export default async function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white"></main>
-  );
+  return <MainPage />;
 }
