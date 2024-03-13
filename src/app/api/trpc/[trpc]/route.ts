56fc020010b29dee920d1396ed70b3114e1b610a
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-03-13 15:46:48 +0700

    maxdur

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -5,10 +5,7 @@ import { env } from "@/env";
 import { appRouter } from "@/server/api/root";
 import { createTRPCContext } from "@/server/api/trpc";
 
-export const config = {
-  maxDuration: 300,
-};
-
+export const maxDuration = 300;
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a HTTP request (e.g. when you make requests from Client Components).
