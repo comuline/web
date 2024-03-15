45e5b3cd35ac6221727f3767307f6a8234ee6569
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-03-16 06:55:01 +0700

    enable flag redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import MainPage from "@/commons/ui/pages/home";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  if (process.env.REDIRECT === "true")
+    return redirect("https://www.comuline.com");
   return <MainPage />;
 }
