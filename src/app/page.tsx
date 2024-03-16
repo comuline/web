a8c35a770fb5b52b2096eed831604d69f3722d33
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-03-16 07:01:34 +0700

    use diff env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import MainPage from "@/commons/ui/pages/home";
+import { env } from "@/env";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  if (process.env.REDIRECT === "true")
+  if (env.NODE_ENV === "production")
     return redirect("https://www.comuline.com");
   return <MainPage />;
 }
