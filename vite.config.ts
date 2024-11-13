a2f2f4d551e21bfdf0e18e4afdcb2a1f21b2b0e3
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-11-13 19:08:03 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import preact from "@preact/preset-vite";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-})
+});
