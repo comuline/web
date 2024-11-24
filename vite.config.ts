d91ff7363a3a626ba516a1023d69ec9db1774ee6
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-11-24 17:26:49 +0700

    fix: resolve alias path vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [preact()],
   build: {
     outDir: ".dist",
