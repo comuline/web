f4af4c905c1347376238a7144a2f89a62575f8a1
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-11-25 16:43:08 +0700

    feat: `useSchedule`

---
+++ src/hooks/use-schedule.ts
@@ -0,0 +1,21 @@
+import { client } from "@/libs/client";
+import { createQueryHook } from "swr-openapi";
+
+/**
+ * Hook to fetch schedule data based on station_id
+ * @param station_id
+ * @example
+ * const { data } = useSchedule("YK");
+ * @returns
+ */
+export const useSchedule = (station_id: string) =>
+  createQueryHook(client, `schedule-${station_id}`)(
+    "/v1/schedule/{station_id}",
+    {
+      params: {
+        path: {
+          station_id,
+        },
+      },
+    },
+  );
