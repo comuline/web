00e1d97d04d380a01aaf6fe9c8ba51ce622b8821
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-11-26 13:12:08 +0700

    fix: create root target

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
