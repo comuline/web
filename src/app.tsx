bbef1481740f7d5ad9e2cab59d308e62f10b5f04
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-11-26 23:06:47 +0700

    feat: wip search

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,7 @@
 import { SearchLg, X } from "@untitled-ui/icons-react";
+import { AnimatePresence, motion, MotionConfig } from "motion/react";
 import React from "react";
 import { StationItem } from "./components/station-item";
-import { cn } from "./utils";
-import { AnimatePresence, motion } from "motion/react";
 import { useMeasure } from "./hooks/use-measure";
 
 const STATION_IDs = ["MRI", "AC"];
@@ -16,60 +15,108 @@ export function App() {
         <nav className="sticky top-0 z-10 flex h-fit flex-col gap-2 bg-white/50 p-5 backdrop-blur-md">
           <AnimatePresence>
             <motion.div animate={{ height }}>
-              <div
-                ref={ref}
-                className={cn(
-                  "flex w-full items-center justify-between gap-3",
-                  {
-                    /*               "pb-[10px]": !isAdding || !isSearching, */
-                  },
-                )}
-              >
-                <AnimatePresence>
-                  {isSearching ? (
-                    <motion.input
-                      transition={{ type: "spring", duration: 0.5, bounce: 0 }}
-                      initial={{ opacity: 0.5, y: 10, filter: "blur(10px)" }}
-                      animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-                      exit={{ opacity: 0.5, y: -10, filter: "blur(10px)" }}
-                      placeholder="Cari stasiun"
-                      className="text-md w-full rounded-md bg-zinc-100 px-2 py-1"
-                    />
-                  ) : (
-                    <motion.div
-                      transition={{ type: "spring", duration: 0.5, bounce: 0 }}
-                      initial={{ opacity: 0.5, y: -10, filter: "blur(10px)" }}
-                      animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-                      exit={{ opacity: 0.5, y: 10, filter: "blur(10px)" }}
-                      className="flex items-center gap-1 py-1 hover:opacity-100"
-                    >
-                      <svg
-                        width="25"
-                        height="25"
-                        viewBox="0 0 250 250"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
+              <div ref={ref} className="flex w-full">
+                <AnimatePresence mode="popLayout" initial={false}>
+                  <MotionConfig
+                    transition={{ type: "spring", duration: 0.8, bounce: 0.3 }}
+                  >
+                    {isSearching ? (
+                      <motion.div
+                        key="search"
+                        initial={{
+                          opacity: 0,
+                          y: -10,
+                          scale: 0.9,
+                          filter: "blur(15px)",
+                        }}
+                        animate={{
+                          opacity: 1,
+                          y: 0,
+                          scale: 1,
+                          filter: "blur(0px)",
+                        }}
+                        exit={{
+                          opacity: 0,
+                          y: 10,
+                          scale: 0.9,
+                          filter: "blur(15px)",
+                        }}
+                        className={
+                          "flex w-full items-center justify-between gap-2 py-0.5 will-change-transform"
+                        }
                       >
-                        <path
-                          d="M91.7793 124.5L122.366 62H200M91.7793 124.5L75.4906 157.784H50M91.7793 124.5L122.366 187H200M91.7793 124.5L75.4906 91.2157H50"
-                          stroke="currentColor"
-                          strokeWidth="20"
+                        <input
+                          placeholder="Cari stasiun"
+                          autoFocus
+                          className="text-md w-full rounded-md bg-zinc-100 px-2 py-1 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-500"
                         />
-                      </svg>
-                      <h1 className="font-mono text-lg tracking-tight">
-                        Comuline
-                      </h1>
-                    </motion.div>
-                  )}
+                        <motion.button
+                          transition={{
+                            type: "spring",
+                            duration: 0.8,
+                            bounce: 0.3,
+                          }}
+                          whileTap={{ scale: 0.75 }}
+                          onClick={() => setIsSearching((prev) => !prev)}
+                          className="rounded-md p-1.5 transition duration-200 ease-in-out hover:bg-zinc-100"
+                        >
+                          <X className="size-5" />
+                        </motion.button>
+                      </motion.div>
+                    ) : (
+                      <motion.div
+                        key="logo"
+                        initial={{
+                          opacity: 0,
+                          y: 10,
+                          scale: 0.9,
+                          filter: "blur(15px)",
+                        }}
+                        animate={{
+                          opacity: 1,
+                          y: 0,
+                          scale: 1,
+                          filter: "blur(0px)",
+                        }}
+                        exit={{
+                          opacity: 0,
+                          y: -10,
+                          scale: 0.9,
+                          filter: "blur(15px)",
+                        }}
+                        className={
+                          "flex w-full items-center justify-between gap-2 will-change-transform"
+                        }
+                      >
+                        <div className="flex items-center gap-1 py-1 hover:opacity-100">
+                          <svg
+                            width="25"
+                            height="25"
+                            viewBox="0 0 250 250"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M91.7793 124.5L122.366 62H200M91.7793 124.5L75.4906 157.784H50M91.7793 124.5L122.366 187H200M91.7793 124.5L75.4906 91.2157H50"
+                              stroke="currentColor"
+                              strokeWidth="20"
+                            />
+                          </svg>
+                          <h1 className="font-mono text-lg tracking-tight">
+                            Comuline
+                          </h1>
+                        </div>
+                        <motion.button
+                          whileTap={{ scale: 0.75 }}
+                          onClick={() => setIsSearching((prev) => !prev)}
+                          className="rounded-md p-1.5 transition duration-200 ease-in-out hover:bg-zinc-100"
+                        >
+                          <SearchLg className="size-5" />
+                        </motion.button>
+                      </motion.div>
+                    )}
+                  </MotionConfig>
                 </AnimatePresence>
-
-                <button onClick={() => setIsSearching((prev) => !prev)}>
-                  {isSearching ? (
-                    <X className="size-5" />
-                  ) : (
-                    <SearchLg className="size-5" />
-                  )}
-                </button>
               </div>
             </motion.div>
           </AnimatePresence>
