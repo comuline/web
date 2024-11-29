692cfbbb8b483bdf7172e014a64983192438c318
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-11-29 15:55:59 +0700

    fix: ui accordion

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -25,13 +25,13 @@ const Trigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all [&[data-state=open]>svg]:rotate-180",
         className,
       )}
       {...props}
     >
       {children}
-      <ChevronDown className="size-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      <ChevronDown className="size-4 shrink-0 text-zinc-500 transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ));
